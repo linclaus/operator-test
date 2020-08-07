b5d26caf0de811678096c1a9277105327392c74a
linzhiqiang <linclaus@outlook.com>
2020-08-07 09:57:16 +0800

    kubebuilder create api

--- main.go
+++ main.go
@@ -82,6 +82,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Guestbook1")
 		os.Exit(1)
 	}
+	if err = (&controllers.Guestbook2Reconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Guestbook2"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Guestbook2")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
